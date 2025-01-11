6647683dab84ad839193c5863f1b4ea45d97c0d1
MartinsChiemezie <146932781+ObikeMartins@users.noreply.github.com>
2025-01-11 17:42:11 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_c6C9KwGqu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '60p2vvsjb5dm18imgcini0jvbr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        clientSecret: '6tssulpk2d0j2hr66gg8j6iman4f964sn20cm2obipajtd2pibi',
+        userPoolClientId: 'NEW_CLIENT_ID_WITHOUT_SECRET', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
